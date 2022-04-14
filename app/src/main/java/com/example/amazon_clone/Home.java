0abc5957fdd6e492f7afeed88958a15d607f50bc
AlejandroGuerra1823 <100006307+AlejandroGuerra1823@users.noreply.github.com>
2022-04-13 21:34:48 -0500

    se sube el product adelantado

--- app/src/main/java/com/example/amazon_clone/Home.java
+++ app/src/main/java/com/example/amazon_clone/Home.java
@@ -1,14 +1,41 @@
 package com.example.amazon_clone;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.os.Bundle;
 
+import com.example.amazon_clone.Adapters.ProductAdapter;
+import com.example.amazon_clone.Entities.Product;
+import com.example.amazon_clone.databinding.ActivityHomeBinding;
+import com.example.amazon_clone.databinding.ActivitySingInBinding;
+
+import java.util.ArrayList;
+
 public class Home extends AppCompatActivity {
+
+    private ActivityHomeBinding jb;
+    private String url;
+    ArrayList <Product> productArrayList;
+    ProductAdapter productAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        jb = ActivityHomeBinding.inflate(getLayoutInflater());
+        setContentView(jb.getRoot());
+        url="";
+        productArrayList = new ArrayList<>();
+        productAdapter = new ProductAdapter(this, productArrayList);
+        jb.rvProducts.setHasFixedSize(true);
+        jb.rvProducts.setLayoutManager(new LinearLayoutManager(this));
+        jb.rvProducts.setAdapter(productAdapter);
+
+
+
+
+
+
+
     }
 }
